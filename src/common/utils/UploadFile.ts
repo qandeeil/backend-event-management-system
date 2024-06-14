fed051e155278f6916ebaac684dfb7658800e624
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-14 14:18:01 +0300

    create class upload file

---
+++ src/common/utils/UploadFile.ts
@@ -0,0 +1,32 @@
+import multer from "multer";
+const path = require("path");
+
+class UploadFile {
+  private storage: multer.StorageEngine;
+  private dir = path.join("public/profile_image");
+
+  constructor() {
+    this.storage = multer.diskStorage({
+      destination: (
+        req: any,
+        file: any,
+        cb: (error: Error | null, destination: string) => void
+      ) => {
+        cb(null, this.dir);
+      },
+      filename: (
+        req: any,
+        file: { originalname: string },
+        cb: (error: Error | null, filename: string) => void
+      ) => {
+        cb(null, Date.now() + "-" + file.originalname);
+      },
+    });
+  }
+
+  public uploadImage() {
+    return multer({ storage: this.storage });
+  }
+}
+
+export default UploadFile;
