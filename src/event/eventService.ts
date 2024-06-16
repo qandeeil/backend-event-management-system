2bcee7d72789acfd8b3260c181d1a0d7b2970397
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-16 23:15:46 +0300

    add getEvents

--- src/event/eventService.ts
+++ src/event/eventService.ts
@@ -1,9 +1,21 @@
 import Event from "./eventModel";
-import { ICreateEvent, IEvent } from "./interfaces";
+import { ICreateEvent } from "./interfaces";
 
 class EventService {
   createEvent(event: ICreateEvent) {
     return Event.create(event);
+  }
+  getEvents(page: number) {
+    const perPage = 5;
+    const skip = (page - 1) * perPage;
+    return Event.find()
+      .skip(skip)
+      .limit(perPage)
+      .select("date location title creator cover_photo price")
+      .populate({
+        path: "creator",
+        select: "name profile_image",
+      });
   }
 }
 
