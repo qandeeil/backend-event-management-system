b522dda6aa664868b38ec64e2f1a433a13c77bfe
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-17 15:13:25 +0300

    add timestamps

--- src/event/eventModel.ts
+++ src/event/eventModel.ts
@@ -1,73 +1,76 @@
 import mongoose from "mongoose";
 import { IEvent } from "./interfaces";
 
-const eventSchema: mongoose.Schema<IEvent> = new mongoose.Schema({
-  title: {
-    type: String,
-    require: [
-      true,
-      JSON.stringify({ name: false, message: "Name is required" }),
-    ],
-  },
-  description: { type: String },
-  date: {
-    start_date: {
-      type: Date,
-      require: [
-        true,
-        JSON.stringify({ start_date: false, message: "Date is required" }),
-      ],
-    },
-    end_date: {
-      type: Date,
-      require: [
-        true,
-        JSON.stringify({ end_date: false, message: "Date is required" }),
-      ],
-    },
-  },
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    require: [
-      true,
-      JSON.stringify({ creator: false, message: "Creator is required" }),
-    ],
-  },
-  location: {
-    country: {
+const eventSchema: mongoose.Schema<IEvent> = new mongoose.Schema(
+  {
+    title: {
       type: String,
       require: [
         true,
-        JSON.stringify({ location: false, message: "Country is required" }),
+        JSON.stringify({ name: false, message: "Name is required" }),
       ],
     },
-    city: {
-      type: String,
+    description: { type: String },
+    date: {
+      start_date: {
+        type: Date,
+        require: [
+          true,
+          JSON.stringify({ start_date: false, message: "Date is required" }),
+        ],
+      },
+      end_date: {
+        type: Date,
+        require: [
+          true,
+          JSON.stringify({ end_date: false, message: "Date is required" }),
+        ],
+      },
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       require: [
         true,
-        JSON.stringify({ location: false, message: "City is required" }),
+        JSON.stringify({ creator: false, message: "Creator is required" }),
       ],
     },
+    location: {
+      country: {
+        type: String,
+        require: [
+          true,
+          JSON.stringify({ location: false, message: "Country is required" }),
+        ],
+      },
+      city: {
+        type: String,
+        require: [
+          true,
+          JSON.stringify({ location: false, message: "City is required" }),
+        ],
+      },
+    },
+    seats: {
+      type: Number,
+      required: [
+        true,
+        JSON.stringify({ seats: false, message: "Seats are required" }),
+      ],
+    },
+    price: {
+      type: Number,
+      required: [
+        true,
+        JSON.stringify({ price: false, message: "Price is required" }),
+      ],
+    },
+    preview_photo: { type: String },
+    cover_photo: { type: String },
+    organizers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
-  seats: {
-    type: Number,
-    required: [
-      true,
-      JSON.stringify({ seats: false, message: "Seats are required" }),
-    ],
-  },
-  price: {
-    type: Number,
-    required: [
-      true,
-      JSON.stringify({ price: false, message: "Price is required" }),
-    ],
-  },
-  preview_photo: { type: String },
-  cover_photo: { type: String },
-  organizers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
+  { timestamps: true }
+);
 
 const Event: mongoose.Model<IEvent> = mongoose.model<IEvent>(
   "Event",
