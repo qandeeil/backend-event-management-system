34f2c54ce8281ff1a5f7184b5570ca24a9f4b61e
Mahmoud Qandeel <99659529+qandeeil@users.noreply.github.com>
2024-06-17 14:16:54 +0300

    ig file public

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "npm",
       args: "start",
       watch: true,
-      ignore_watch: ["node_modules", "public/profile_image"],
+      ignore_watch: ["node_modules", "public"],
       env: {
         NODE_ENV: "production",
         PORT: 7001,
